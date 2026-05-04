4549584768f98657b7c2bf6bf3b896b62167a106
baguesai <baguesai@bagues.net.test-google-a.com>
2026-05-04 13:21:22 -0300

    fix server crash

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require("http");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("custom_test_1 funcionando 🚀");
-  res.end("DEPLOY AUTOMÁTICO FUNCIONANDO... OK 🔥");
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+  res.end(`
+    <h1>custom_test_1 funcionando 🚀</h1>
+  `);
 });
 
-server.listen(PORT, "127.0.0.1");
+server.listen(PORT, "127.0.0.1", () => {
+  console.log(`Servidor en http://127.0.0.1:${PORT}`);
+});
