0bdfff37a1f14fe7a9b9cd6f2fe46617bf0d5c70
baguesai <baguesai@bagues.net.test-google-a.com>
2026-05-04 12:24:01 -0300

    init custom_test_1

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const PORT = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("custom_test_1 funcionando 🚀");
+});
+
+server.listen(PORT, "127.0.0.1");
